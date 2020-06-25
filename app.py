e88271ef017d21bcd62def8a01ea45db7a1289cb
hun340312 <hun340312@naver.com>
2020-06-26 02:22:09 +0900

    textfile process ver 0.6.1

--- app.py
+++ app.py
@@ -14,6 +14,142 @@ import math
 import nltk
 from nltk import word_tokenize
 
+import pprint
+from elasticsearch import Elasticsearch
+
+
+word_d = {}
+sent_list = []
+arr = []
+result = []
+wordList =[]
+TF = []
+wordCount = [0]*10
+processTime = [0]*10
+
+
+
+
+def clean_str(text):
+	pattern = '([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)' # E-mail제거
+	text = re.sub(pattern=pattern, repl='', string=text)
+	pattern = '(http|ftp|https)://(?:[-\w.]|(?:%[\da-fA-F]{2}))+' # URL제거
+	text = re.sub(pattern=pattern, repl='', string=text)
+	pattern = '([ㄱ-ㅎㅏ-ㅣ]+)'  # 한글 자음, 모음 제거
+	text = re.sub(pattern=pattern, repl='', string=text)
+	pattern = '<[^>]*>'         # HTML 태그 제거
+	text = re.sub(pattern=pattern, repl='', string=text)
+	pattern = '[^\w\s]'         # 특수기호제거
+	text = re.sub(pattern=pattern, repl='', string=text)
+	pattern = '[0-9]'
+	text = re.sub(pattern=pattern, repl='', string=text)
+	
+	return text   
+
+def process_new_sentence(s):
+	
+	
+	sent_list.append(s)
+	tokenized = word_tokenize(s)
+	for word in tokenized:
+		word = word.lower()	
+		if word not in word_d.keys():
+			word_d[word]=0
+		word_d[word] += 1
+
+def compute_tf(s):
+	bow = set()
+	# dictionary for words in the given sentence (document)
+	wordcount_d = {}
+
+	tokenized = word_tokenize(s)
+	for tok in tokenized:
+		tok = tok.lower()	
+		if tok not in wordcount_d.keys():
+			wordcount_d[tok]=0
+		wordcount_d[tok] += 1
+		bow.add(tok)
+	tf_d = {}
+	for k,v in wordcount_d.items():
+		tf_d[k] = v/(float(len(bow)))
+	return tf_d
+	
+	
+def compute_idf():
+	Dval = len(sent_list)
+	bow = set()
+	for i in range(0,len(sent_list)):
+		tokenized = word_tokenize(clean_str(sent_list[i].lower()))
+		for tok in tokenized:
+			bow.add(tok)
+	idf_d = {}
+	for t in bow:
+		cnt = 1
+		for s in sent_list:
+			if t in word_tokenize(s.lower()):
+				cnt += 1
+			idf_d[t]=math.log((float(len(bow)))/cnt)
+	return idf_d
+	
+	
+def main(url):
+
+	req = urllib.request.Request(url)
+	sourcecode = urllib.request.urlopen(url).read()
+	soup = BeautifulSoup(sourcecode, "html.parser")
+	str = soup.select_one('div#bodyColumn')
+
+		
+	para = str.text.strip()
+	start = timeit.default_timer()
+	docs = para.split('\n')	
+	v = []
+
+	sent_list.clear()
+	for line in docs:
+		process_new_sentence(line)
+		
+	idf_d = compute_idf()
+		
+	for i in range(0,len(sent_list)):
+		tf_d = compute_tf(clean_str(sent_list[i]))
+		
+	gather = " ".join(sent_list)
+	tf_d = compute_tf(clean_str(gather))
+	dic_print = {}
+	dic_print.clear()
+	
+	for word,tfval in tf_d.items():
+		dic_print[word] = tfval*idf_d[word]
+		
+	arr = sorted(dic_print.items(),key=lambda x: x[1], reverse=True)
+		
+	count = 0
+	wordList.clear()
+	TF.clear()
+	
+	for x,y in arr:
+		if(count==10):break
+		wordList.insert(count,x)
+		TF.insert(count,y)
+		count +=1		
+	
+	end = timeit.default_timer()
+	end -= start
+
+	"""try:
+		es = Elasticsearch('127.0.0.1:9200')
+		
+		data = {"url" : url, "words" : wordList.copy(), "TF_IDF values" : TF.copy()}
+		res = es.index(index = 'web',doc_type = "word", body = data)
+		print(data)
+		print("\n")
+		pprint.pprint(res)
+	except KeyboardInterrupt:
+		pass"""
+	
+	
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -32,119 +168,24 @@ def getURL():
 def getFILE():
 	return render_template('getFILE.html')
 
+@app.route("/printFILE/",methods = ['post'])
+def printFILE():	
+	URLs = request.form['URLs']
+	URLarr = URLs.split()
+	print(URLarr)	
+	return render_template('printFILE.html',)
+
 @app.route("/printURL/", methods = ['post'])
 def printURL():
 
-	word_d = {}
-	sent_list = []
-	arr = []
-	result = []
-
-	def clean_str(text):
-    		pattern = '([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)' # E-mail제거
-    		text = re.sub(pattern=pattern, repl='', string=text)
-    		pattern = '(http|ftp|https)://(?:[-\w.]|(?:%[\da-fA-F]{2}))+' # URL제거
-    		text = re.sub(pattern=pattern, repl='', string=text)
-    		pattern = '([ㄱ-ㅎㅏ-ㅣ]+)'  # 한글 자음, 모음 제거
-    		text = re.sub(pattern=pattern, repl='', string=text)
-    		pattern = '<[^>]*>'         # HTML 태그 제거
-    		text = re.sub(pattern=pattern, repl='', string=text)
-    		pattern = '[^\w\s]'         # 특수기호제거
-    		text = re.sub(pattern=pattern, repl='', string=text)
-    		pattern = '[0-9]'
-    		text = re.sub(pattern=pattern, repl='', string=text)
-    		return text   
-
-	def process_new_sentence(s):
-		sent_list.append(s)
-		tokenized = word_tokenize(s)
-		for word in tokenized:
-			word = word.lower()	
-			if word not in word_d.keys():
-				word_d[word]=0
-			word_d[word] += 1
-
-	def compute_tf(s):
-		bow = set()
-		# dictionary for words in the given sentence (document)
-		wordcount_d = {}
-
-		tokenized = word_tokenize(s)
-		for tok in tokenized:
-			tok = tok.lower()	
-			if tok not in wordcount_d.keys():
-				wordcount_d[tok]=0
-			wordcount_d[tok] += 1
-			bow.add(tok)
-		tf_d = {}
-		for k,v in wordcount_d.items():
-			tf_d[k] = v/(float(len(bow)))
-		return tf_d
-	
-	
-	def compute_idf():
-		Dval = len(sent_list)
-		bow = set()
-		for i in range(0,len(sent_list)):
-			tokenized = word_tokenize(clean_str(sent_list[i].lower()))
-			for tok in tokenized:
-				bow.add(tok)
-		idf_d = {}
-		for t in bow:
-			cnt = 1
-			for s in sent_list:
-				if t in word_tokenize(s.lower()):
-					cnt += 1
-				idf_d[t]=math.log((float(len(bow)))/cnt)
-		return idf_d
-	
-	
-	def main():
-	
-		url = request.form['userURL']
-		req = urllib.request.Request(url)
-		sourcecode = urllib.request.urlopen(url).read()
-		soup = BeautifulSoup(sourcecode, "html.parser")
-		str = soup.select_one('div#bodyColumn')
-
-		
-		para = str.text.strip()
-		start = timeit.default_timer()
-		docs = para.split('\n')
-		
-		v = []
-		for line in docs:
-			process_new_sentence(line)
-		
-		idf_d = compute_idf()
-		
-		for i in range(0,len(sent_list)):
-			tf_d = compute_tf(clean_str(sent_list[i]))
-		
-		gather = " ".join(sent_list)
-		tf_d = compute_tf(clean_str(gather))
-		dic_print = {}
-	
-		for word,tfval in tf_d.items():
-			dic_print[word] = tfval*idf_d[word]
-		arr = sorted(dic_print.items(),key=lambda x: x[1], reverse=True)
-		
-		count = 0
-		for x,y in arr:
-			if(count==10):break
-			result.append(x)
-			result.append(y)
-			count +=1		
-	
-		end = timeit.default_timer()
-		end -= start
+	url = request.form['userURL']	
 	
 	start = timeit.default_timer()
-	main()
+	main(url)
 	end = timeit.default_timer()
 	end -= start
 	
-	return render_template('printURL.html', word1 = result[0], value1 = result[1], word2 = result[2], value2 = result[3],word3 = result[4], value3 = result[5],word4 = result[6], value4 = result[7],word5 = result[8], value5 = result[9],word6 = result[10], value6= result[11],word7 = result[12], value7 = result[13],word8 = result[14], value8 = result[15],word9 = result[16], value9 = result[17],word10 = result[18], value10 = result[19], time = end)
+	return render_template('printURL.html', url = url, word1 = wordList[0], value1 = TF[0], word2 = wordList[1], value2 = TF[1],word3 = wordList[2], value3 = TF[2],word4 = wordList[3], value4 = TF[3],word5 = wordList[4], value5 = TF[4],word6 = wordList[5], value6= TF[5],word7 = wordList[6], value7 = TF[6],word8 = wordList[7], value8 = TF[7],word9 = wordList[8], value9 = TF[8],word10 = wordList[9], value10 = TF[9], time = end)
 
 @app.route("/wordAnal/")
 def wordAnal():
